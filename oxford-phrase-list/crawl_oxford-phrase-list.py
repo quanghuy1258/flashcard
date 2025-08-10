05a2f16e42cb439096ef881731dff11f899b3618
Nguyen Van Quang Huy <quanghuy1258@gmail.com>
2025-08-10 11:36:13 +0000

    update

--- oxford-phrase-list/crawl_oxford-phrase-list.py
+++ oxford-phrase-list/crawl_oxford-phrase-list.py
@@ -8,6 +8,10 @@ soup = bs4.BeautifulSoup(r.text, "html.parser")
 f = open("oxford-phrase-list.db", "w", encoding="utf-8")
 f.write("index,phrase,belong_to,definition\n")
 
+corrected_definitions = {}
+corrected_definitions["https://www.oxfordlearnersdictionaries.com/definition/english/time_1#"] = "https://www.oxfordlearnersdictionaries.com/definition/english/time_1#time_sng_35"
+corrected_definitions["https://www.oxfordlearnersdictionaries.com/definition/english/near_3#pretty_sng_13"] = "https://www.oxfordlearnersdictionaries.com/definition/english/pretty_1#pretty_sng_13"
+
 for index, value in enumerate(soup.select("#wordlistsContentPanel li")):
   # phrase
   phrase = str(value.a.string)
@@ -15,6 +19,8 @@ for index, value in enumerate(soup.select("#wordlistsContentPanel li")):
   belong_to = str(value.div.span.string)
   # definition
   definition = prefix_url + value.a["href"]
+  if definition in corrected_definitions:
+    definition = corrected_definitions[definition]
   # insert words into db
   f.write("{},{},{},{}\n".format(index,
                                  phrase,
