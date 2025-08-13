38fecac586b1b5b11a2d30949f9ec535e82e6b9d
Van-Quang-Huy Nguyen <quanghuy1258@gmail.com>
2025-08-13 07:52:45 +0700

    Update create_oxford-phrase-list_anki_deck.py

--- oxford-phrase-list/create_oxford-phrase-list_anki_deck.py
+++ oxford-phrase-list/create_oxford-phrase-list_anki_deck.py
@@ -26,7 +26,7 @@ f.write("#deck column:2\n")
 f.write("#tags column:5\n")
 
 template_string = "Basic\toxford-phrase-list\t\"Belong to: {belong_to}<br><a href=\"\"{definition}\"\">Definition</a><br><div style=\"\"text-align: center;\"\"><b>{phrase}</b></div>\"\t\"Phrase #{index:03d}<br>{note}\"\t\n"
-note_string = "<ol><li>This is the first meaning.</li><ul><li>This is the first example of the first meaning.</li><li>This is the second one. To do this, we use Ordered List (1, 2, 3, ...) for meanings, then Increase Indent (in Alignment) and use Unordered List for examples.</li></ul><li>This is the second meaning. To do this, we may use Decrease Indent (in Alignment) first.</li><ul><li>This is the second example of the first meaning.</li><li>This is second one. Sometimes, we need to hide keywords. If we want to do that, for example keyword = {}, we can write keyword = ___.</li></ul></ol>"
+note_string = "<ol><li>This is the first meaning.</li><ul><li>This is the first example of the first meaning.</li><li>This is the second one. To do this, we use Ordered List (1, 2, 3, ...) for meanings, then Increase Indent (in Alignment) and use Unordered List for examples.</li></ul><li>This is the second meaning. To do this, we may use Decrease Indent (in Alignment) first.</li><ul><li>This is the second example of the first meaning.</li><li>This is the second one. Sometimes, we need to hide keywords. If we want to do that, for example keyword = {}, we can write keyword = ___.</li></ul></ol>"
 for index, row in db.iterrows():
   note_s = note_string.format(row["phrase"])
   if str(row["index"]) in note:
