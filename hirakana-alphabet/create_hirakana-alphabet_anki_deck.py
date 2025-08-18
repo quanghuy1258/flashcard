933b2dd4d8746ee38f2d28976e19dab4e4e2778b
Nguyen Van Quang Huy <quanghuy1258@gmail.com>
2025-08-18 12:01:53 +0000

    update

---
+++ hirakana-alphabet/create_hirakana-alphabet_anki_deck.py
@@ -0,0 +1,31 @@
+import shutil
+from os import listdir, makedirs
+
+media_folder = "COPY_ALL_MEDIA_FILES_HERE_TO_THE_COLLECTION.MEDIA_FOLDER"
+makedirs(media_folder, exist_ok=True)
+
+f = open("hirakana-alphabet-no-audio.txt", "w", encoding="utf-8")
+f.write("#separator:tab\n")
+f.write("#html:true\n")
+f.write("#notetype column:1\n")
+f.write("#deck column:2\n")
+f.write("#tags column:5\n")
+
+template_string = "Basic (type in the answer)\thirakana-alphabet-no-audio\t\"{character}<br><img src=\"\"{image}\"\" style=\"\"max-width: 40%; height: auto;\"\">\"\t{label}\t\n"
+
+for code in listdir("db"):
+  label_txt = [filename for filename in listdir("db/{}".format(code)) if "label_" in filename][0]
+  with open("db/{}/{}".format(code, label_txt), encoding="utf-8") as label_file:
+    hira, kana, en = label_file.read().splitlines()
+  hira_img = "test_hira_{}.png".format(label_txt[6:-4])
+  kana_img = "test_kana_{}.png".format(label_txt[6:-4])
+  f.write(template_string.format(character = hira,
+                                 image = hira_img,
+                                 label = en))
+  f.write(template_string.format(character = kana,
+                                 image = kana_img,
+                                 label = en))
+  shutil.copy("db/{}/{}".format(code, hira_img), "{}/{}".format(media_folder, hira_img))
+  shutil.copy("db/{}/{}".format(code, kana_img), "{}/{}".format(media_folder, kana_img))
+
+f.close()
