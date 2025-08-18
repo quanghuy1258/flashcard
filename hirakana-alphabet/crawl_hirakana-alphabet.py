76cd144d1df26e5faa86b5c20534b4a8b52e3471
Nguyen Van Quang Huy <quanghuy1258@gmail.com>
2025-08-18 07:49:12 +0000

    update

--- hirakana-alphabet/crawl_hirakana-alphabet.py
+++ hirakana-alphabet/crawl_hirakana-alphabet.py
@@ -1,11 +1,31 @@
 import os.path
 import pathlib
-import requests
 import subprocess
 import itertools
-
+import json
+import requests
 import scrapy
 from PIL import Image
+
+name2hirakana = {}
+def hira_kana_json(code):
+  url_template = "https://iknow.jp/api/v2/goals/{}"
+  response = requests.get(url_template.format(code), headers={"User-agent": "Mozilla/5.0"})
+  if response.status_code != 200:
+    raise Exception("Error code: {}. Please check url = {}".format(response.status_code, url_template.format(code)))
+  return json.loads(response.content)["goal_items"]
+for hira_item, kana_item in zip(hira_kana_json(24666), hira_kana_json(24667)):
+  hira = hira_item["item"]["cue"]["text"]
+  kana = kana_item["item"]["cue"]["text"]
+  en = hira_item["item"]["response"]["text"]
+  assert en == kana_item["item"]["response"]["text"]
+  if en == "di":
+    name = "ji2.png"
+  elif en == "du":
+    name = "zu2.png"
+  else:
+    name = "{}.png".format(en)
+  name2hirakana[name] = {"hira": hira, "kana": kana, "en": en}
 
 white = (255, 255, 255)
 standard_size = (880, 739)
@@ -54,4 +74,9 @@ for node in sel.xpath('//div[@class="swiper-slide"]'):
   im = Image.alpha_composite(background, im.resize(standard_size))
   im.paste(white, paste_pos)
   im.save("db/{:03d}/test_kana_{}".format(code, name))
+
+  with open("db/{:03d}/label_{}.txt".format(code, name[:-4]), "w", encoding="utf-8") as f:
+    f.write("{}\n".format(name2hirakana[name]["hira"]))
+    f.write("{}\n".format(name2hirakana[name]["kana"]))
+    f.write("{}\n".format(name2hirakana[name]["en"]))
   print("{:03d} {}".format(code, name))
